c7adb006edda012a758164858d3fc8dd05dbc470
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-24 07:52:47 +0000

    Deploy bipface/galkontinuum to github.com/bipface/galkontinuum.git:master

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 # Galkontinuum
 Galkontinuum is a [userscript](https://en.wikipedia.org/wiki/Userscript) which enables slideshow-style browsing of search results on the Booru family of websites.
+It targets galleries running Gelbooru 0.2.x, Danbooru 2.x, Danbooru 1.x and compatible forks such as e621 and Moebooru.
--- dist/galkontinuum.user.js
+++ dist/galkontinuum.user.js
@@ -58,7 +58,7 @@ const manifest = {
 const readmeMarkdown = `
 # Galkontinuum
 Galkontinuum is a [userscript](https://en.wikipedia.org/wiki/Userscript) which enables slideshow-style browsing of search results on the Booru family of websites.
-`;
+It targets galleries running Gelbooru 0.2.x, Danbooru 2.x, Danbooru 1.x and compatible forks such as e621 and Moebooru.`;
 
 /*
 
