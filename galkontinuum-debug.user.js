442ecc152f7e2c23a3c36f7cb0ddfcc15a6d6352
bipface <bipface@users.noreply.github.com>
2019-04-24 17:51:42 +1000

    Update galkontinuum-debug.user.js

--- galkontinuum-debug.user.js
+++ galkontinuum-debug.user.js
@@ -27,7 +27,7 @@ const manifest = null; /* will be assigned manfiest object in release-mode */
 const readmeMarkdown = `
 # Galkontinuum
 Galkontinuum is a [userscript](https://en.wikipedia.org/wiki/Userscript) which enables slideshow-style browsing of search results on the Booru family of websites.
-`;
+It targets galleries running Gelbooru 0.2.x, Danbooru 2.x, Danbooru 1.x and compatible forks such as e621 and Moebooru.`;
 
 /*
 
